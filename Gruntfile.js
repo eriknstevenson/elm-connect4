7030b6b7c68cffe86685f9a50aaefa03af035592
erik stevenson <eriknstevenson@gmail.com>
2015-10-15 20:05:14 -0400

    modified gruntfile and package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,35 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    elm: {
-      compile: {
-        files: {
-          "connect4.js": ["connect4.elm"]
-        }
-      }
-    },
-    watch: {
-      elm: {
-        files: ["connect4.elm"],
-        tasks: ["elm"]
-      }
-    },
-    clean: ["elm-stuff/build-artifacts"]
-  });
+    grunt.initConfig({
+        uglify: {
+            my_target: {
+                files: {
+                    'connect4.js': ['connect4.js']
+                }
+            }
+        },
+        elm: {
+            compile: {
+                files: {
+                    "connect4.js": ["connect4.elm"]
+                }
+            }
+        },
+        watch: {
+            elm: {
+                files: ["connect4.elm"],
+                tasks: ["elm"]
+            }
+        },
+        clean: ["elm-stuff/build-artifacts"]
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-elm');
+    });
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-elm');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['elm']);
+    grunt.registerTask('default', ['elm']);
+    grunt.registerTask('release', ['clean','elm','uglify']);
 
 };
