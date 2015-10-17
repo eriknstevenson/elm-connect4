069d684d4a893ca5e243a2ba6c74b9fe77e0d149
erik stevenson <eriknstevenson@gmail.com>
2015-10-16 20:14:24 -0400

    Create View.elm

---
+++ Connect4/View.elm
@@ -0,0 +1,38 @@
+module Connect4.View (view) where
+
+-- VIEW
+
+view : S.Address Action -> Model -> Html
+view address model = 
+  let
+    makeSpace : Space -> Html
+    makeSpace space =
+      case space.status of
+        Empty -> div [ class "empty",
+                 onClick address (MakeMove space.id) ] []
+        Red -> div [class "x"]
+          [text "X"]
+        Blue -> div [class "o"]
+          [text "O"]
+    
+    winnerToString : Maybe Player -> String
+    winnerToString winner =
+      case winner of
+        Nothing -> "Nobody won.. -.-"
+        Just Player1 -> "Player 1 won! XD"
+        Just Player2 -> "Player 2 won! XD"
+
+  in
+    div [] 
+      [ div [id "board"] (L.map makeSpace model.board)
+      , div [class "notice"] 
+          (case model.gameOver of
+            False -> [text "Connect 4"]
+            True ->
+              [ ul []
+                [ li [] [text "game over"]
+                , li [] [text <| winnerToString model.winner]
+                , li [] [text "click refresh to try again"] ]
+              ]
+          )
+        ]
